93bc34eb20736e9d1b2a1e0344249ec52cd38355
Andrew Butterworth <andrew.butterworth@cern.ch>
2023-03-05 03:41:28 +0100

    Using ESPSoftwareSerial

--- src/main.cpp
+++ src/main.cpp
@@ -1,82 +1,74 @@
 #include <Arduino.h>
-#include <HardwareSerial.h>
+//#include <HardwareSerial.h>
+#include <SoftwareSerial.h>
+
 #include <sys/time.h>
 
 #include <iomanip>
 #include <ios>
 #include <sstream>
 
-HardwareSerial uart1(1);
-//HardwareSerial uart2(2);
+//#define CONTROL_FLAG 0x01
 
-int64_t get_time_us();
-int32_t get_elapsed_time();
+SoftwareSerial uart1;
 
 void setup() {
     // set up serial ports:
     Serial.begin(921600);
-    uart1.begin(9600, SERIAL_8N1, 4, 2);
-    //uart2.begin(9600, SERIAL_8N1, 16, 17);
-    // interrupt on every received byte:
-    uart1.setRxFIFOFull(1);
-    //uart2.setRxFIFOFull(1);
+    Serial.setTimeout(2000);
+    uart1.begin(9600, SWSERIAL_8S1, 23, 19, false);
+    //uart1.setRxFIFOFull(1);
 
     // clear the UART buffers
     while (uart1.available() > 0) {
         uart1.read();
     }
-    while (uart2.available() > 0) {
-        uart2.read();
-    }
 }
 
 void loop() {
 
-    int byte1 = 0;
-    int byte2 = 0;
-    bool byte1Received = false;
-    bool byte2Received = false;
+    int txdelay = 0;
+    bool rxinv = false;
+    String str = Serial.readStringUntil('\n');
+    if (str.length() > 0) {
+        str.trim();
+        Serial.println(str);
+        for (int i = 1; i < str.length(); i += 2) {
+            int byte = std::stoi(std::string(str.substring(i, i + 2).c_str()), 0, 16);
+            // uart1.write(str.c_str()[i]);
+            Serial.println(str.substring(i, i + 2));
+            //uart1.end();
+            //uart1.begin(9600, SERIAL_8N1, 4, 2);
+            // rxinv = !rxinv;
+            // uart1.setRxInvert(rxinv);
 
-    while (!(byte1Received || byte2Received)) {
-        // read the UART channels
-        if (uart1.available() > 0) {
-            byte1 = uart1.read();
-            byte1Received = true;
-        }
-
-        if (uart2.available() > 0) {
-            byte2 = uart2.read();
-            byte2Received = true;
+            uart1.write(byte, SWSERIAL_PARITY_SPACE);
+            //uart1.flush();
+            if (str[0] == '*') {
+                txdelay = 1500;
+            } else {
+                txdelay = 500;
+            }
+            usleep(txdelay);
         }
     }
+    // while (!(byte1Received /* || byte2Received */)) {
+    //     // read the UART channels
+    //     if (uart1.available() > 0) {
+    //         byte1 = uart1.read();
+    //         byte1Received = true;
+    //     }
 
-    int elapsed_time = get_elapsed_time();
+    // }
+
+    //int elapsed_time = get_elapsed_time();
 
     // print the formatted output: byte1 byte2 elapsed_time (us)
-    std::ostringstream stream("", std::ios_base::app);
-    if (byte1Received)
-        stream << std::setfill('0') << std::setw(2) << std::hex << byte1;
-    else
-        stream << "..";
-    stream << " ";
-    if (byte2Received)
-        stream << std::setfill('0') << std::setw(2) << std::hex << byte2;
-    else
-        stream << "..";
-    stream << " " << std::setfill(' ') << std::setw(6) << std::dec << elapsed_time;
-    Serial.println(stream.str().c_str());
-}
-
-int64_t get_time_us() {
-    struct timeval tv_now;
-    gettimeofday(&tv_now, NULL);
-    return (int64_t)tv_now.tv_sec * 1000000L + (int64_t)tv_now.tv_usec;
-}
-
-int get_elapsed_time() {
-    static int64_t timestamp = 0;
-    int64_t now = get_time_us();
-    int64_t elapsed_time = now - timestamp;
-    timestamp = now;
-    return (int)std::min(elapsed_time, (int64_t)999999);
+    // std::ostringstream stream("", std::ios_base::app);
+    // if (byte1Received)
+    //     stream << std::setfill('0') << std::setw(2) << std::hex << byte1;
+    // else
+    //     stream << "..";
+    // stream << " ";
+    // Serial.println(stream.str().c_str());
 }
